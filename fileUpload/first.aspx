5debdaf51d15f1eb07857e1ed255d4c017b1ecab
pradeep budhathoki <pradeep.budhathoki1234@gmail.com>
2017-09-17 11:49:47 +0545

    another commit

---
+++ fileUpload/first.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="first.aspx.cs" Inherits="fileUpload.first" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <asp:Button ID="Button1" runat="server" Height="21px" Text="Upload" OnClick="Button1_Click" />
+        </div>
+        <p>
+            <asp:Label ID="Label1" runat="server" Font-Bold="true"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ fileUpload/first.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace fileUpload
+{
+    public partial class first : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                String extension = System.IO.Path.GetExtension(FileUpload1.FileName);
+
+                if (extension.ToLower() != ".txt" && extension.ToLower() != ".doc")
+                {
+                    Label1.Text = "only txt or docx files!!";
+                    Label1.ForeColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    int filesize = FileUpload1.PostedFile.ContentLength;
+                    if (filesize > 2097152)
+                    {
+
+                        Label1.Text = "only less than 2 mb";
+                        Label1.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("~/uploads/" + FileUpload1.FileName));
+                        Label1.Text = "file uploaded!!";
+                        Label1.ForeColor = System.Drawing.Color.Green;
+                    }
+                }
+            }
+            else
+            {
+                Label1.Text = "select a file to upload!!";
+                Label1.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+    }
+}
